b9f4a3c5330ce0a6b8d1769e54f95f5c51ed78ec
Kore Nordmann <mail@kore-nordmann.de>
2012-03-13 10:06:30 +0100

    Added: Common set of rewrite rules

---
+++ src/app/rewrites.js
@@ -0,0 +1,34 @@
+[
+   {
+       "from": "/",
+       "to": "index.html"
+   },
+   {
+       "from": "/styles/*",
+       "to": "styles/*"
+   },
+   {
+       "from": "/scripts/*",
+       "to": "scripts/*"
+   },
+   {
+       "from": "/images/*",
+       "to": "images/*"
+   },
+   {
+       "from": "/templates/*",
+       "to": "templates/*"
+   },
+   {
+       "from": "/show*",
+       "to": "_show/*"
+   },
+   {
+       "from": "/list/*",
+       "to": "_list/*"
+   },
+   {
+       "from": "/*",
+       "to": "/index.html"
+   }
+]
